86f4ef4adc4b5b2566eb2e8108f41f84d334872b
mohitsawhney111 <56771997+mohitsawhney111@users.noreply.github.com>
2019-10-26 11:14:43 -0400

    ver 1.1

--- testscript.sh
+++ testscript.sh
@@ -2,6 +2,6 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-
+echo "branch created ver 1.1 hooked up on commit changes"
 echo "this was run automagically"
-echo "hooked up on commit changes"
+echo "hooked up on commit changes"echo "branch created ver 1.1 hooked up on commit changes"
